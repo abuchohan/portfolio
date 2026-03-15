b2f715337e6452698e4d8ebc7eb563b0ddffbf97
Abu-Hurairah Chohan <abuchohan@hotmail.co.uk>
2026-03-15 16:21:24 +0000

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
-
+// import FloatingLines from '
+import { Button } from "@/components/ui/button";
+import FloatingLines from "@/components/FloatingLines";
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <div style={{ width: "100%", height: "600px", position: "relative" }}>
+        <FloatingLines
+          enabledWaves={["top", "middle", "bottom"]}
+          // Array - specify line count per wave; Number - same count for all waves
+          lineCount={5}
+          // Array - specify line distance per wave; Number - same distance for all waves
+          lineDistance={5}
+          bendRadius={5}
+          bendStrength={-0.5}
+          interactive={true}
+          parallax={true}
+        />
+      </div>
+      <Button />
+    </>
+  );
 }
 
 export default App;
